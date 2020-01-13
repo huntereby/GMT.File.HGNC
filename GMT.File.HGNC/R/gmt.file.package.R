a6a349265667e16ab103ab9df958e79fb762bce7
huntereby <56884427+huntereby@users.noreply.github.com>
2020-01-13 14:47:13 -0500

    Add files via upload

---
+++ GMT.File.HGNC/R/gmt.file.package.R
@@ -0,0 +1,11 @@
+#'Read GMT. FILE
+#'
+#'Provides user with HGNC Mouse_hgnc2.gmt File for fgsea use
+#'
+#'@parms there are no variables
+#'@exprot
+
+function(){
+  GMT.file <- gmtPathways("Mouse_hgnc2.gmt")
+}
+
---
+++ GMT.File.HGNC/R/package1.R
@@ -0,0 +1,11 @@
+#' Read GMT. FILE
+#'
+#'HGNC provides gene familes, we have curtaed them to gene sets used for GSEA, this package simply loads a gmt file used in R shiny
+#'
+#'@docType package
+#'
+#'@author Hunter Eby \email {huntereby66@gmial.com}
+#'
+#'@name GMT.File.HGNC
+NULL
+
